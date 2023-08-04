b79278fb3edaa569a613fab6db5c26133a338afd
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-08-03 22:51:06 -0400

    first commit

---
+++ pv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *.jpg | *.jpeg | *.png | *.gif | *.ico) ~/.config/lf/lf_kitty_preview.sh "$1" "$2" "$3" "$4" "$5";;
+    *) highlight -O ansi "$1";;
+esac
